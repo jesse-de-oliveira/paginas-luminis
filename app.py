aaf8fa7b5b3a9b4006afaea6a1ca2d994a227ac7
Jesse De Oliveira <jessedeoliveira@edu.unifil.br>
2026-08-26 20:25:50 +0200

    Prepara projeto para deploy: adiciona gunicorn, psycopg2 e suporte a DATABASE_URL

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', '3f9a8b2c1d4e7f')
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL',
+    'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 from models import db, Usuario, Livro, StatusLeitura 
